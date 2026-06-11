4286a7ebee7026bbf863fb6b3fbe1dd30793b89d
fakhrulhasan99 <fakhrulhasan.faf1@gmail.com>
2026-06-11 22:42:15 +0600

    app and server created

---
+++ src/app.ts
@@ -0,0 +1,143 @@
+import express, { type Application, type Request, type Response } from "express"
+import { pool } from "./database";
+const app: Application = express()
+
+app.use(express.json());
+
+app.get('/', (req: Request, res: Response) => {
+  res.status(200).json({
+    "message": "express server",
+    "author": "fakhrul"
+  })
+})
+
+app.post("/api/auth/signup", async (req: Request, res: Response) => {
+  // console.log(req.body.name)
+  const { name, email, password, role } = req.body;
+  try {
+    const result = await pool.query(`
+    INSERT INTO users(name, email, password, role) VALUES($1,$2,$3,$4) RETURNING *`, [name, email, password, role]);
+    // we should not return password
+    // we should not return password
+    // we should not return password
+    // we should not return password
+    // we should not return password
+    // console.log(result)
+    res.status(201).json({
+      success: true,
+      message: "User registered successfully",
+      data: result.rows[0]
+    })
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error
+    })
+  }
+});
+
+app.get("/api/auth/signup", async (req: Request, res: Response) => {
+  try {
+    const result = await pool.query(`
+      SELECT * FROM users`);
+    res.status(200).json({
+      success: true,
+      message: "Users retrived successfully",
+      data: result.rows
+    })
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error
+    })
+  }
+});
+
+app.get("/api/auth/signup/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  try {
+    const result = await pool.query(`
+      SELECT * FROM users WHERE id=$1`, [id]);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+        data: {}
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User retrived successfully",
+      data: result.rows[0]
+    })
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error
+    })
+  }
+});
+
+app.put("/api/auth/signup/:id", async (req: Request, res: Response) => {
+  const id = req.params.id;
+  const { name, password, role } = req.body;
+  try {
+    const result = await pool.query(`
+      UPDATE users SET name=COALESCE($1,name), password=COALESCE($2,password), role=COALESCE($3,role) WHERE id=$4 RETURNING *`, [name, password, role, id]);
+
+    if (result.rows.length === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+        data: {}
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User updated successfully",
+      data: result.rows[0]
+    })
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error
+    })
+  }
+});
+
+app.delete("/api/auth/signup/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+  try {
+    const result = await pool.query(`
+      DELETE FROM users WHERE id=$1`, [id]);
+
+    if (result.rowCount === 0) {
+      res.status(404).json({
+        success: false,
+        message: "User not found",
+        data: {}
+      })
+    }
+
+    res.status(200).json({
+      success: true,
+      message: "User deleted successfully",
+      data: {}
+    })
+  } catch (error: any) {
+    res.status(500).json({
+      success: false,
+      message: error.message,
+      error: error
+    })
+  }
+});
+
+export default app;
